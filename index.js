a8191a16141fd681cd2c51d88a39b4c11b5f678d
awesomak <awesomebest2@gmail.com>
2020-07-30 17:18:03 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const server = require("http").createServer();
 
-const io = require("socket.io").listen(server);
-
-io.set("transports", ["websocket"]);
+const io = require("socket.io").listen(server, { transports: ["websocket"] });
 
 var spawn = require("child_process").spawn;
 
