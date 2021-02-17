d55e86aa0ea56cf1cec486609e27fa5f1fe98c7c
awesomak <awesomebest2@gmail.com>
2021-02-17 13:39:09 +0200

    new config

--- index.js
+++ index.js
@@ -79,23 +79,41 @@ io.on("connection", function (socket) {
     var ops = [
       "-i",
       "-",
-      //'-c', 'copy',
+
+      // video codec config: low latency, adaptive bitrate
       "-c:v",
       "libx264",
       "-preset",
-      "ultrafast",
+      "veryfast",
       "-tune",
-      "zerolatency", // video codec config: low latency, adaptive bitrate
+      "zerolatency",
+
+      // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
       "-c:a",
       "aac",
+      "-strict",
+      "-2",
       "-ar",
-      audioBitrate,
+      "44100",
       "-b:a",
-      audioEncoding, // audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+      "64k",
+
+      //force to overwrite
+      "-y",
+
+      // used for audio sync
+      "-use_wallclock_as_timestamps",
+      "1",
+      "-async",
+      "1",
+
+      //'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
+      //'-strict', 'experimental',
       "-bufsize",
-      "5000",
+      "1000",
       "-f",
       "flv",
+
       socket._rtmpDestination,
     ];
 
